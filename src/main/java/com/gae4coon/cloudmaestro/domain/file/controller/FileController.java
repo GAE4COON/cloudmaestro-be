035776f2ded8d28750b4492071beb25a7005330a
ssoxong <ssoxong@naver.com>
2023-10-14 14:54:07 +0900

    Feat: json format

--- src/main/java/com/gae4coon/cloudmaestro/domain/file/controller/FileController.java
+++ src/main/java/com/gae4coon/cloudmaestro/domain/file/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.gae4coon.cloudmaestro.domain.file.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gae4coon.cloudmaestro.domain.file.dto.InputData;
 import com.gae4coon.cloudmaestro.domain.file.dto.NodeData;
@@ -8,6 +9,7 @@ import com.gae4coon.cloudmaestro.domain.file.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -48,20 +50,68 @@ public class FileController {
 
     @PostMapping(value = "/summary")
     public ResponseEntity<?> summaryFile(@RequestParam("file") MultipartFile file) throws IOException {
+//        if (file.isEmpty()) {
+//            return ResponseEntity.badRequest().body("File is empty.");
+//        }
+//
+//        // 파일의 내용을 문자열로 읽기
+//        String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+//
+//        // JSON 문자열을 DTO로 변환
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        InputData inputData = objectMapper.readValue(content, InputData.class);
+//        OutputData outputData = transformData(inputData);
         if (file.isEmpty()) {
             return ResponseEntity.badRequest().body("File is empty.");
         }
 
-        // 파일의 내용을 문자열로 읽기
-        String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+        try {
+            // Read the content of the uploaded file into a string
+            String content = new String(file.getBytes(), StandardCharsets.UTF_8);
 
-        // JSON 문자열을 DTO로 변환
-        ObjectMapper objectMapper = new ObjectMapper();
+            // Parse the JSON string into a Map<String, Object>
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> jsonData = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
+            });
 
-        InputData inputData = objectMapper.readValue(content, InputData.class);
-        OutputData outputData = transformData(inputData);
+            // 변환된 데이터를 담을 결과 맵
+            Map<String, Object> response = new HashMap<>();
+            Map<String, Object> costMap = (Map<String, Object>) jsonData.get("cost");
 
-        return ResponseEntity.ok(outputData);
+            // "compute" 부분 변환
+            Map<String, Object> compute = new HashMap<>();
+            System.out.println("key!!!!!!!!"+costMap.keySet());
+            for (String key : costMap.keySet()) {
+                if (key.startsWith("EC2")) {
+                    compute.put(key, costMap.get(key));
+                }
+            }
+            response.put("compute", compute);
+
+            // "database" 부분 변환
+            Map<String, Object> database = new HashMap<>();
+            for (String key : costMap.keySet()) {
+                if (key.startsWith("RDS")) {
+                    database.put(key, costMap.get(key));
+                }
+            }
+            response.put("database", database);
+
+            // "storage" 부분 변환
+            Map<String, Object> storage = new HashMap<>();
+            for (String key : costMap.keySet()) {
+                if (key.startsWith("Simple Storage Service")) {
+                    storage.put(key, costMap.get(key));
+                }
+            }
+            response.put("storage", storage);
+
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            // Handle exceptions such as IO errors or JSON parsing errors
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing the file: " + e.getMessage());
+        }
     }
 
 
@@ -88,13 +138,17 @@ public class FileController {
                 Map<String, Object> comDetails = new HashMap<>();
                 if (matchedCost.containsKey("platform")) {
                     comDetails.put("platform", matchedCost.get("platform"));
-                }if (matchedCost.containsKey("instancetype")) {
+                }
+                if (matchedCost.containsKey("instancetype")) {
                     comDetails.put("instancetype", matchedCost.get("instancetype"));
-                }if (matchedCost.containsKey("size")) {
+                }
+                if (matchedCost.containsKey("size")) {
                     comDetails.put("size", matchedCost.get("size"));
-                }if (matchedCost.containsKey("billing")) {
+                }
+                if (matchedCost.containsKey("billing")) {
                     comDetails.put("billing", matchedCost.get("billing"));
-                }if (matchedCost.containsKey("cost")) {
+                }
+                if (matchedCost.containsKey("cost")) {
                     comDetails.put("cost", matchedCost.get("cost"));
                 }
                 compute.put(nodeData.getKey(), comDetails);
@@ -103,11 +157,14 @@ public class FileController {
                 Map<String, Object> dbDetails = new HashMap<>();
                 if (matchedCost.containsKey("engine")) {
                     dbDetails.put("engine", matchedCost.get("engine"));
-                }if (matchedCost.containsKey("instancetype")) {
+                }
+                if (matchedCost.containsKey("instancetype")) {
                     dbDetails.put("instancetype", matchedCost.get("instancetype"));
-                }if (matchedCost.containsKey("size")) {
+                }
+                if (matchedCost.containsKey("size")) {
                     dbDetails.put("size", matchedCost.get("size"));
-                }if (matchedCost.containsKey("cost")) {
+                }
+                if (matchedCost.containsKey("cost")) {
                     dbDetails.put("cost", matchedCost.get("cost"));
                 }
                 // Add other attributes if needed...
