9ce01e7c7503d2cf512b5755bfa76b29064c50d6
soooochan <nowee018@naver.com>
2023-10-29 21:46:34 +0900

    feat : localhost:3000

--- src/main/java/com/gae4coon/cloudmaestro/global/config/SpringCors.java
+++ src/main/java/com/gae4coon/cloudmaestro/global/config/SpringCors.java
@@ -9,6 +9,7 @@ public class SpringCors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
