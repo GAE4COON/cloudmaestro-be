f8cd3e5ca15c5e458f35287110b07fe85d73c1ab
hellokms8828 <hellokms8828@naver.com>
2023-11-27 20:51:27 +0900

    Feat: vpc alert

--- src/main/java/com/gae4coon/cloudmaestro/domain/alert/controller/CheckController.java
+++ src/main/java/com/gae4coon/cloudmaestro/domain/alert/controller/CheckController.java
@@ -1,7 +1,15 @@
 package com.gae4coon.cloudmaestro.domain.alert.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gae4coon.cloudmaestro.domain.alert.dto.inputDto;
 import com.gae4coon.cloudmaestro.domain.alert.service.DiagramCheckService;
+import com.gae4coon.cloudmaestro.domain.requirements.dto.RequireDTO;
+import com.gae4coon.cloudmaestro.domain.ssohost.dto.GraphLinksModel;
+import com.gae4coon.cloudmaestro.domain.ssohost.dto.GroupData;
 import com.gae4coon.cloudmaestro.domain.ssohost.dto.LinkData;
+import com.gae4coon.cloudmaestro.domain.ssohost.dto.NodeData;
+import com.gae4coon.cloudmaestro.domain.ssohost.service.DiagramDTOService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/alert-api")
@@ -17,6 +27,7 @@ import java.util.HashMap;
 public class CheckController {
 
     private final DiagramCheckService diagramCheckService;
+    private final DiagramDTOService diagramDTOService;
 
     @PostMapping("/alert-check")
     public ResponseEntity<HashMap<String, Object>> alertCheck(@RequestBody LinkData postData) {
@@ -26,11 +37,48 @@ public class CheckController {
         HashMap result = new HashMap<>();
         try {
 
-        HashMap ResponseMap = diagramCheckService.linkCheck(postData);
-        result.put("result", ResponseMap);
-        return ResponseEntity.ok().body(result);
+            HashMap ResponseMap = diagramCheckService.linkCheck(postData);
+            result.put("result", ResponseMap);
+            return ResponseEntity.ok().body(result);
 
-        }catch (Exception e){
+        } catch (Exception e) {
+            result.put("result", "error");
+            System.out.println("Error: " + e.getMessage());
+            return ResponseEntity.ok().body(result);
+        }
+    }
+    @PostMapping("/node-check")
+    public ResponseEntity<Object> nodeCheck(@RequestBody inputDto inputData) throws JsonProcessingException {
+        HashMap result = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println("inputData: "+inputData);
+        try {
+            if (inputData.getDiagramData()!=null) {
+                GraphLinksModel diagramData = mapper.readValue(inputData.getDiagramData(), GraphLinksModel.class);
+                System.out.println("diagramData:" + diagramData);
+                // diagramData formatter
+                Map<String, Object> responseArray = diagramDTOService.dtoGenerator(diagramData);
+
+                List<NodeData> nodeDataList = (List<NodeData>) responseArray.get("nodeDataArray");
+                List<GroupData> groupDataList = (List<GroupData>) responseArray.get("groupDataArray");
+                List<LinkData> linkDataList = (List<LinkData>) responseArray.get("linkDataArray");
+
+                if (inputData.getCheckOption().equals("VPC")) {
+                    HashMap ResponseMap = diagramCheckService.vpcCheck(groupDataList);
+                    result.put("result", ResponseMap);
+                } else {
+                    HashMap<String, String> check = new HashMap<>();
+                    check.put("status", "success");
+                    result.put("result", check);
+                }
+            }else {
+                HashMap<String, String> check = new HashMap<>();
+                check.put("status", "success");
+                result.put("result", check);
+            }
+
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
             result.put("result", "error");
             System.out.println("Error: " + e.getMessage());
             return ResponseEntity.ok().body(result);
--- src/main/java/com/gae4coon/cloudmaestro/domain/alert/service/DiagramCheckService.java
+++ src/main/java/com/gae4coon/cloudmaestro/domain/alert/service/DiagramCheckService.java
@@ -1,9 +1,11 @@
 package com.gae4coon.cloudmaestro.domain.alert.service;
 
+import com.amazonaws.services.ec2.model.Vpc;
+import com.gae4coon.cloudmaestro.domain.ssohost.dto.GroupData;
 import com.gae4coon.cloudmaestro.domain.ssohost.dto.LinkData;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.*;
 
 @Service
 public class DiagramCheckService {
@@ -16,6 +18,100 @@ public class DiagramCheckService {
             check.put("status", "success");
         }
 
+        return check;
+    }
+
+
+    public List<String> BPgroupSearch(List<GroupData> groupDataList) {
+        HashSet<String> BPSet = new HashSet<>();
+        for (GroupData group : groupDataList) {
+            if(group.getText().equals("Module")){
+                BPSet.add(group.getKey());
+            }
+        }
+
+        int previousSize = 0;
+        int currentSize = BPSet.size();
+
+        while (previousSize != currentSize) {
+            previousSize = currentSize;
+
+            for (GroupData groupItem : groupDataList) {
+                if (groupItem.getGroup() instanceof String) {
+                    for (String setItem : BPSet) {
+                        if (groupItem.getGroup().equals(setItem)) {
+                            BPSet.add(groupItem.getKey());
+                        }
+                    }
+                }
+            }
+            currentSize = BPSet.size();
+        }
+        return new ArrayList<>(BPSet);
+    }
+
+
+
+
+    public HashMap<String, String> vpcCheck(List<GroupData> groupDataList) {
+        boolean flag = false;
+        HashMap<String, String> check = new HashMap<>();
+        List<GroupData> checkgr = new ArrayList<>();
+        Map<String, List<String>> regionVPC = new HashMap<>();
+
+
+        if(groupDataList != null) {
+            //BP 찾기
+            List<String> BPgroup=BPgroupSearch(groupDataList);
+            System.out.println(BPgroup);
+
+
+            for (GroupData group : groupDataList) {
+                //그룹 체크
+                if (group.getGroup() != null) {
+                    if (group.getText().equals("VPC") && (!group.getGroup().contains("Region") && !BPgroup.contains(group.getKey()))) {
+                        flag=true;
+                        check.put("message", "VPC는 Region 그룹 안에 있어야 합니다.");
+                        break;
+                    }
+                }
+                //그룹에 잘 있다면? 그리고 Region 거르기
+                else{
+                    if (group.getText().equals("Region")) {
+                        checkgr.add(group);
+                    }
+                }
+            }
+            if(flag != true) {
+                for (GroupData region : checkgr) {
+                    List<String> vpcKeys = new ArrayList<>();
+                    for (GroupData group : groupDataList) {
+                        if (region.getKey().equals(group.getGroup())) {
+                            vpcKeys.add(group.getKey());
+                        }
+                    }
+                    regionVPC.put(region.getKey(), vpcKeys);
+                }
+
+                for (Map.Entry<String, List<String>> entry : regionVPC.entrySet()) {
+                    String key = entry.getKey(); // 키를 얻음
+                    List<String> vpcs = entry.getValue(); // VPC 리스트
+
+                    if (vpcs.size()>5){
+                        flag=true;
+                        check.put("message", "VPC는 한 Region당 5개를 초과할 수 없습니다.");
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        if (flag) {
+            check.put("status", "fail");
+        }else{
+            check.put("status", "success");
+        }
         return check;
     }
 }
