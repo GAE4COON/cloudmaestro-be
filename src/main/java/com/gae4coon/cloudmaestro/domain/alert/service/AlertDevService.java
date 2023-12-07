20e40a45279b6addae2c3bf84f182e287ce2b6ad
soooochan <nowee018@naver.com>
2023-12-07 15:35:54 +0900

    file 삭제

---
+++ src/main/java/com/gae4coon/cloudmaestro/domain/alert/service/AlertDevService.java
@@ -0,0 +1,18 @@
+package com.gae4coon.cloudmaestro.domain.alert.service;
+
+import com.gae4coon.cloudmaestro.domain.ssohost.dto.GroupData;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlertDevService {
+//    public List<String> alertDev(List<GroupData> groupDataList) {
+//        for(GroupData groupData : groupDataList){
+//            if(groupData.getGroup().contains("dev") || groupData.getGroup().contains("DEV")){
+//
+//            }
+//        }
+//
+//    }
+}
