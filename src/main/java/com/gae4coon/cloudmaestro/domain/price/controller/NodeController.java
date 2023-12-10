db6a3a4a1e2911494d086a62f8072dd766632630
ssoxong <ssoxong@naver.com>
2023-12-10 22:20:35 +0900

    init

--- src/main/java/com/gae4coon/cloudmaestro/domain/price/controller/NodeController.java
+++ src/main/java/com/gae4coon/cloudmaestro/domain/price/controller/NodeController.java
@@ -21,40 +21,29 @@ import lombok.RequiredArgsConstructor;
 
 public class NodeController {
 
-    private static final Logger logger = LoggerFactory.getLogger(NodeController.class);
-
     private NodeService nodeService;
+
+    @Autowired
+    private dbService dbConnectService;
+
 
     @GetMapping("/all")
     public List<MemberDTO> getAllMembers() {
         List<MemberDTO> members = nodeService.getAllMembers();
-        logger.info("Fetched members: {}", members);
-        return members;
-    }
-
-    public List<MemberDTO> getMembersByAPIName(@PathVariable("APIName") String APIName) {
-        List<MemberDTO> members = nodeService.getMembersByAPIName(APIName);
-        logger.info("Fetched members by name '{}': {}", APIName, members);
         return members;
     }
 
     @PostMapping(value = "/ec2", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public List<String> getMembersByAPIName(@RequestBody ApiNameRequest request) {
         List<String> splitAPIname = null;
-        logger.info("hello world '{}': {}", request.getPlatform(), request.getInstanceType());
         if(request.getInstanceType() == null) {
             // Only platform provided
             splitAPIname = nodeService.getSplitByAPIName(request.getPlatform());
-            logger.info("Fetched members by platform '{}': {}", request.getPlatform(), splitAPIname);
         } else {
             splitAPIname = nodeService.getSplitByAPINameAndInstanceType(request.getPlatform(), request.getInstanceType());
-            logger.info("Fetched members by instance '{}': {}", request.getInstanceType(), splitAPIname);
         }
         return splitAPIname;
     }
-
-    @Autowired
-    private dbService dbConnectService;
     @PostMapping("/rds")
     public Map<String, List<String>> getDbData(@RequestBody Map<String, String> payload) {
         String engine = payload.get("engine");
